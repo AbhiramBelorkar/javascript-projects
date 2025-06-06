214d8b6762fd1a2feebd41ed48aa5c04c93731e3
abhiramb <abhiramb@cdac.in>
2025-06-06 11:37:25 +0530

    slider

--- js-slider/slider.js
+++ js-slider/slider.js
@@ -1,20 +1,64 @@
 const left = document.querySelector(".left")
 const right = document.querySelector(".right")
 const slider = document.querySelector(".slider")
-const image = document.querySelectorAll(".image")
+const images = document.querySelectorAll(".image")
+const bottom = document.querySelector('.bottom')
 
-let length = 1
-let imageLength = image.length
+let sliderLength = 1
+const imageNo = images.length
+// console.log(imageNo);
 
-left.addEventListener('click', () => {
-    if(length < imageLength){
-        slider.style.transform = `translateX(-${length * 800}px)`
-        length++
-        console.log("Inside if" ,length);
-
+right.addEventListener('click', () => {
+    if (sliderLength < imageNo) {
+        slider.style.transform = `translateX(-${sliderLength * 800}px)`
+        console.log(sliderLength);
+        sliderLength++
     } else {
-        length = 0;
-        console.log(length);
+        slider.style.transform = `translateX(-0px)`
+        sliderLength = 1;
     }
+    changeColor()
 
 })
+
+left.addEventListener('click', () => {
+    if (sliderLength > 1) {
+        slider.style.transform = `translateX(-${(sliderLength - 2) * 800}px)`;
+        sliderLength--
+    } else {
+        slider.style.transform = `translateX(-${(imageNo - 1) * 800}px)`
+        sliderLength = imageNo
+    }
+    changeColor()
+
+})
+
+for (let i = 0; i < imageNo; i++) {
+    const div = document.createElement('div')
+    div.className = "button"
+    // console.log(div);
+    bottom.appendChild(div)
+}
+
+const buttons = document.querySelectorAll(".button")
+buttons[0].style.backgroundColor = "white"
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        removeBg()
+        slider.style.transform = `translateX(-${(index) * 800}px)`
+        console.log(index);
+        button.style.backgroundColor = 'white'
+    })
+})
+
+const removeBg = () => {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = 'transparent'
+    })
+}
+
+const changeColor = () => {
+    removeBg()
+    buttons[sliderLength - 1].style.backgroundColor = "white";
+}
